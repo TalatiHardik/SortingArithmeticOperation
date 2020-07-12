e6288b05678029f066775dbc67645ca8142a14c7
TalatiHardik <hardikhtalati@gmail.com>
2020-07-12 17:53:04 +0530

    UC4 completed

--- index.sh
+++ index.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash -x
-echo "Welcome to UC3 -Computer a*b+c"
+echo "Welcome to UC4 -Computer c+a/b"
 read -p "Enter input number 1 " a
 read -p "Enter input number 2 " b
 read -p "Enter input number 3 " c
@@ -10,3 +10,6 @@ echo " $a + $b * $c = $operation1"
 
 operation2=$(( $a * $b + $c ))
 echo " $a * $b + $c = $operation2"
+
+operation3=$(( $c + $a / $b ))
+echo " $c + $a / $b = $operation3"
