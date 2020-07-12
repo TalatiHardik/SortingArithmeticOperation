9457bf3cd0bef951f3703e45f5ca9c46bbef5a3d
TalatiHardik <hardikhtalati@gmail.com>
2020-07-12 18:08:09 +0530

    UC6 completed

--- index.sh
+++ index.sh
@@ -1,18 +1,27 @@
 
 #!/bin/bash -x
-echo "Welcome to UC5 -Computer a%b+c"
+
+declare -A arithemtic
+
+echo "Welcome to UC6 -Store reuslt in dictonary"
 read -p "Enter input number 1 " a
 read -p "Enter input number 2 " b
 read -p "Enter input number 3 " c
 
 operation1=$(( $a + $b * $c ))
 echo " $a + $b * $c = $operation1"
+arithmetic["a+b*c"]=$operation1
 
 operation2=$(( $a * $b + $c ))
 echo " $a * $b + $c = $operation2"
+arithmetic["a*b+c"]=$operation2
 
 operation3=$(( $c + $a / $b ))
 echo " $c + $a / $b = $operation3"
+arithmetic["c+a/b"]=$operation3
 
 operation4=$(( $a % $b + $c ))
 echo " $a % $b + $c = $operation4"
+arithmetic["a%b+c"]=$operation4
+
+#echo ${arithmetic[@]}
