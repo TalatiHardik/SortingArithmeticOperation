ec8a181e3da88a5d84568f86149ef5b50d0cb9be
TalatiHardik <hardikhtalati@gmail.com>
2020-07-12 17:31:41 +0530

    program edited

--- index.sh
+++ index.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash -x
 echo "Welcome to UC1 -Taking input a b and c"
-read -p "Enter input number 1" a
-read -p "Enter input number 1" b
-read -p "Enter input number 1" c
+read -p "Enter input number 1 " a
+read -p "Enter input number 2 " b
+read -p "Enter input number 3 " c
