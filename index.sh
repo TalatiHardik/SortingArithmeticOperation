52d7f684510d6abf5db1be3b437448a08a0dbb51
TalatiHardik <hardikhtalati@gmail.com>
2020-07-12 17:38:13 +0530

    Usecase 2 completed

--- index.sh
+++ index.sh
@@ -1,6 +1,9 @@
 
 #!/bin/bash -x
-echo "Welcome to UC1 -Taking input a b and c"
+echo "Welcome to UC2 -Computer a+b*c"
 read -p "Enter input number 1 " a
 read -p "Enter input number 2 " b
 read -p "Enter input number 3 " c
+
+operation1=$(( $a + $b * $c ))
+echo " $a + $b * $c = $operation1"
